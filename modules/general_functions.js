d5ade9476f0f3810578e25a65a69c038fa270813
Hamza Dahmoun <ham.dahmoun@fcmg-cnac.dz>
2020-02-05 14:53:49 +0100

    add general_functions.js to use, & write divide() func & export default

---
+++ modules/general_functions.js
@@ -0,0 +1,18 @@
+//this js file is supposed to contain many functions but only one of them is going to be exported
+// ==> we'll use export default
+
+export default divide = (x, y) => {
+    return x / y;
+}
+
+function functionName() {
+    return "just a test";
+}
+
+function functionName() {
+    return "just a test";
+}
+
+function functionName() {
+    return "just a test";
+}
