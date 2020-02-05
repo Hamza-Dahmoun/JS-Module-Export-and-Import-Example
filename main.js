c804ea544f41447ebe10b1a34a2e4819cbe1ed4d
Hamza Dahmoun <ham.dahmoun@fcmg-cnac.dz>
2020-02-05 14:58:39 +0100

    fix a bug in general_functions.js & use it in main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-import * as myMathModule from "./modules/math_functions.js";
-
 //in this file we're going to import functions from math_functions.js file and use them
+
+
+import * as myMathModule from "./modules/math_functions.js";
+import divide from "./modules/general_functions.js";
+
+
 
 let x = 10;
 let y = 5;
@@ -10,3 +14,5 @@ console.log("The sum is: " + myMathModule.add(x, y));
 console.log("The subtraction is: " + myMathModule.subtract(x, y));
 
 console.log("The production is: " + myMathModule.multiply(x, y));
+
+console.log("The division is: " + divide(x, y));
--- modules/general_functions.js
+++ modules/general_functions.js
@@ -1,18 +1,18 @@
 //this js file is supposed to contain many functions but only one of them is going to be exported
 // ==> we'll use export default
 
-export default divide = (x, y) => {
+export default function divide(x, y){
     return x / y;
 }
 
-function functionName() {
+function functionName1() {
     return "just a test";
 }
 
-function functionName() {
+function functionName2() {
     return "just a test";
 }
 
-function functionName() {
+function functionName3() {
     return "just a test";
 }
