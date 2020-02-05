d3312c9e2d5208ef2ea1a56c306d8de7137cb419
Hamza Dahmoun <ham.dahmoun@fcmg-cnac.dz>
2020-02-05 14:26:39 +0100

    write add, subtract & multiply functions & export them

---
+++ modules/math_functions.js
@@ -0,0 +1,16 @@
+//this is addition operation
+const add = (x, y) => {
+    return x + y;
+}
+
+//this is subtraction operation
+const subtract = (x, y) => {
+    return x - y;
+}
+
+//this is multiply operation
+const multiply = (x, y) => {
+    return x * y;
+}
+
+export { add, subtract, multiply };
